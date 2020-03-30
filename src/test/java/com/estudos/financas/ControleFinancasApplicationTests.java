5074233ab6ced937bd2c8147b6b2cdb79c73a0da
Evandson <evandson.cm@gmail.com>
2020-03-29 23:14:31 -0300

    Primeiro Commit

---
+++ src/test/java/com/estudos/financas/ControleFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
